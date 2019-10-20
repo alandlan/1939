c8fa693fb32070c205d746d55d876a2eac85e2bb
Alan Vieira Martins <alanhorcrux@gmail.com>
2019-10-20 16:12:52 -0300

    Create class Livro, Autor e Categoria

---
+++ BookStores/BookStores/Models/Livro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStores.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string ISBN { get; set; }
+        public DateTime DataLancamento { get; set; }
+
+    }
+}
