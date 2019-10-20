1e81e6c7959bc53b2ba0aef11d4871a0041d164a
Alan Vieira Martins <alanhorcrux@gmail.com>
2019-10-20 20:05:12 -0300

    Add new migrations

---
+++ BookStores/BookStores/Context/BookStoreDataContext.cs
@@ -0,0 +1,22 @@
+﻿using BookStores.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BookStores.Context
+{
+    public class BookStoreDataContext : DbContext
+    {
+        public BookStoreDataContext()
+            :base("BookStoreConnectionString")
+        {
+
+        }
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Livro> Livros { get; set; }
+
+    }
+}
