b8c232069cdd64447a47cdb688298a04333ebd2a
Alan Vieira Martins <alanhorcrux@gmail.com>
2019-10-24 22:12:58 -0300

    Removido migrations e criado Controller Teste e Index

---
+++ BookStores/BookStores/ViewModels/VincularLivroAutorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStores.ViewModels
+{
+    public class VincularLivroAutorViewModel
+    {
+        [Required(ErrorMessage = "*")]
+        public int AutorId { get; set; }
+        [Required(ErrorMessage = "*")]
+
+        public int LivroId { get; set; }
+    }
+}
