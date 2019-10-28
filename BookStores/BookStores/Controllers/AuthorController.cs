f10350dbc85673a63c12ad88f9a06f78ac5855c4
Alan Vieira Martins <alanhorcrux@gmail.com>
2019-10-27 23:00:38 -0300

    Controller Autor

---
+++ BookStores/BookStores/Controllers/AuthorController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStores.Controllers
+{
+    [RoutePrefix("autor")]
+    public class AuthorController : Controller
+    {
+        [Route("listar")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("criar")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [Route("editar")]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [Route("deletar")]
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
