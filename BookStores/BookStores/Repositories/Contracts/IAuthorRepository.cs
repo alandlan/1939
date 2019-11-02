a4ddd2c94819a711e70bbbbb92e7e1c30315f8c9
Alan Vieira Martins <alanhorcrux@gmail.com>
2019-11-02 18:26:16 -0300

    Criado interfaces para Autores

---
+++ BookStores/BookStores/Repositories/Contracts/IAuthorRepository.cs
@@ -0,0 +1,18 @@
+﻿using BookStores.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStores.Repositories.Contracts
+{
+    public interface IAuthorRepository : IDisposable
+    {
+        List<Autor> Get();
+        Autor Get(int id);
+        Autor GetByName(string name);
+        bool Create(Autor autor);
+        bool Update(Autor autor);
+        void Delete(int id);
+    }
+}
