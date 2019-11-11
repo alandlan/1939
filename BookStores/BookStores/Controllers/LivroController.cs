ac4275281fa6e90c35fe50735dff5b1853012438
Alan Vieira Martins <alanhorcrux@gmail.com>
2019-11-10 22:54:56 -0300

    Ajuste controller Livro

--- BookStores/BookStores/Controllers/LivroController.cs
+++ BookStores/BookStores/Controllers/LivroController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BookStores.Context;
+using BookStores.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,26 @@ namespace BookStores.Controllers
     [RoutePrefix("Livros")]
     public class LivroController : Controller
     {
+        readonly BookStoreDataContext _bookStore = new BookStoreDataContext();
         // GET: Livro
         [Route("criar")]
         public ActionResult Create()
+        {
+            var categorias = _bookStore.Categorias.ToList();
+            var model = new CreateBookViewModel
+            {
+                Nome = "",
+                ISBN = "",
+                CategoriaId = 0,
+                CategoriaOptions = new SelectList(categorias, "Id", "Nome")
+            };
+
+            return View(model);
+        }
+
+        [Route("criar")]
+        [HttpPost]
+        public ActionResult Create(CreateBookViewModel modelBook)
         {
             return View();
         }
--- BookStores/BookStores/ViewModels/CreateBookViewModel.cs
+++ BookStores/BookStores/ViewModels/CreateBookViewModel.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace BookStores.ViewModels
 {
     public class CreateBookViewModel
     {
+        [Required(ErrorMessage ="*")]
+        [Display(Name = "Nome do Livro")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "*")]
+        public string ISBN { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Data de Lançamento")]
+        [DataType(DataType.Date)]
+        public DateTime DataLancamento { get; set; }
+        [Required(ErrorMessage = "*")]
+        public int CategoriaId { get; set; }
+
+        public SelectList CategoriaOptions { get; set; }
     }
 }
